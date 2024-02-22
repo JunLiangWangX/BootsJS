4df70bb5d213f3286ec294973f198bdcc92695b9
wangjl <1428637884@qq.com>
2024-02-22 21:54:19 +0800

    init test

--- __test__/priority-queue.test.js
+++ __test__/priority-queue.test.js
@@ -1,21 +1,26 @@
-// import demand(按需导入)
 const { PriorityQueue } = require('../dist/priority-queue');
-// global import(全局导入)
 const EnhanceJS=require('../dist/index');
 
-/**
- * 浏览器环境
- * 按需
- * <script src='../dist/priority-queue'></script>
- * 全局
- * <script src='../dist/index'></script>
- * 
- * <script>
- * let priorityQueue = new window.EnhanceJS.PriorityQueue();
- * </script>
- */
 
-test('test PriorityQueue Class', () => {
+test('test PriorityQueue class', () => {
+    /**
+     * 浏览器环境
+     * 
+     * 按需
+     * <script src='priority-queue.js'></script>
+     * 全局
+     * <script src='enhance-js.js'></script>
+     * <script>
+     * let priorityQueue = new window.EnhanceJS.PriorityQueue();
+     * </script>
+     * 
+     * node环境
+     * 按需
+     * const { PriorityQueue } = require('../dist/priority-queue');
+     * 全局导入
+     * const EnhanceJS=require('../dist/index');
+     */
+
     let priorityQueue = new PriorityQueue();
     priorityQueue.enqueue('1', 1)
     priorityQueue.enqueue('1', 5)
