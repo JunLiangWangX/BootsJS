afb0b4c69c690a5f2277c68caabcaba93433b6f3
JunLiangWangX <JunLiangWangX@users.noreply.github.com>
2024-02-23 14:51:12 +0000

    deploy: 047c2856bc73143de656701944443f261f095e9d

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKKMrML8osqQwsTS1NVdJRKkgsyVCyUkrOSSwuTi3WR5HWyyjJzVHSUcrOzEtRsjI0sqiNBQAzVBQISQAAAA=="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKKMrML8osqQwsTS1NVdJRKkgsyVCyUkrOSSwuTi3WR5HWyyjJzVHSUcrOzEtRsjI0sqjVgZsTlJoeVJqTWoxpBEwGU3csABt+9a+HAAAA"
