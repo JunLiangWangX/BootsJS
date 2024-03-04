7acec55bfc8817ac4c07f33abb2e3c36832f872d
汪军梁 <1428637884@qq.com>
2024-03-04 11:32:50 +0800

    perfect:modify the four trim methods into one

--- __test__/string-tool.test.js
+++ __test__/string-tool.test.js
@@ -4,9 +4,9 @@ const BootsJS=require('../dist/index');
 
 test('test StringTool class', () => {
     expect(StringTool.dashNameToUpperCamelCaseName('string-tool')).toBe('StringTool');
-    expect(BootsJS.StringTool.camelCaseNameToDashName('StringTool')).toBe('string-tool');
-    expect(BootsJS.StringTool.trimLeft(' StringTool')).toBe('StringTool');
-    expect(BootsJS.StringTool.trimRight('StringTool ')).toBe('StringTool');
-    expect(BootsJS.StringTool.trimSide(' StringTool ')).toBe('StringTool');
-    expect(BootsJS.StringTool.trimAll(' String Tool ')).toBe('StringTool');
+    expect(StringTool.camelCaseNameToDashName('StringTool')).toBe('string-tool');
+    expect(StringTool.trim(' StringTool',StringTool.trimOptions.leading)).toBe('StringTool');
+    expect(StringTool.trim('StringTool ',StringTool.trimOptions.trailing)).toBe('StringTool');
+    expect(StringTool.trim(' StringTool ',StringTool.trimOptions.side)).toBe('StringTool');
+    expect(StringTool.trim(' String Tool ')).toBe('StringTool');
 });
--- src/string-tool.ts
+++ src/string-tool.ts
@@ -3,9 +3,18 @@
  * @Author: JunLiangWang
  * @Date: 2024-02-28 15:00:37
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2024-02-28 16:46:26
+ * @LastEditTime: 2024-03-04 11:28:59
  */
 
+/**
+ * TrimOptions(去除空格选项)
+ */
+enum TrimOptions {
+  all,
+  side,
+  leading,
+  trailing
+}
 /**
  * Some methods of processing strings.(一些处理字符串的方法)
  * 
@@ -21,112 +30,79 @@
  * StringTool.dashNameToUpperCamelCaseName('string-tool')
  *  ```
  */
-export class StringTool{
-    /**
-     * @description: Convert dash naming to camel case naming.(短横线命名转大写驼峰命名)
-     * @param {string} dashName The dash name that needs to be converted.(需要转换的短横线名称)
-     * @example 
-     * StringTool.dashNameToUpperCamelCaseName('string-tool') //StringTool
-     */  
-    static dashNameToUpperCamelCaseName(dashName:string):string {
-        return dashName.replace(/-([a-zA-Z])/g, function (match, letter) {
-          return letter.toUpperCase();
-        }).replace(/^[a-z]/, function (letter) {
-          return letter.toUpperCase();
-        });
-      }
-    /**
-     * @description: Convert camel case naming to dash naming.(驼峰命名转短横线命名)
-     * @param {string} camelCaseName The camel case name that needs to be converted.(需要转换的驼峰命名)
-     * @example 
-     * StringTool.camelCaseNameToDashName('StringTool') //string-tool
-     */    
-    static camelCaseNameToDashName(camelCaseName:string):string {
-        return camelCaseName.replace(/^[A-Z]/, function (letter) {
-          return letter.toLowerCase();
-        }).replace(/[A-Z]/g, function (letter) {
-           return '-'+letter.toLowerCase();
-        });
-      }
+export class StringTool {
+  /**
+   * @description: Convert dash naming to camel case naming.(短横线命名转大写驼峰命名)
+   * @param {string} dashName The dash name that needs to be converted.(需要转换的短横线名称)
+   * @example 
+   * StringTool.dashNameToUpperCamelCaseName('string-tool') //StringTool
+   */
+  static dashNameToUpperCamelCaseName(dashName: string): string {
+    return dashName.replace(/-([a-zA-Z])/g, function (match, letter) {
+      return letter.toUpperCase();
+    }).replace(/^[a-z]/, function (letter) {
+      return letter.toUpperCase();
+    });
+  }
+  /**
+   * @description: Convert camel case naming to dash naming.(驼峰命名转短横线命名)
+   * @param {string} camelCaseName The camel case name that needs to be converted.(需要转换的驼峰命名)
+   * @example 
+   * StringTool.camelCaseNameToDashName('StringTool') //string-tool
+   */
+  static camelCaseNameToDashName(camelCaseName: string): string {
+    return camelCaseName.replace(/^[A-Z]/, function (letter) {
+      return letter.toLowerCase();
+    }).replace(/[A-Z]/g, function (letter) {
+      return '-' + letter.toLowerCase();
+    });
+  }
 
-    /**
-     * @description: 判断是否是字符串
-     * @param {any} str
-     * @example 
-     *  this.#checkIsString(' StringTool') // true
-     *  this.#checkIsString(1) // false
-     * @returns {boolean} 类型是否是字符串
-     */ 
-
-      static #checkIsString(str:unknown):str is string{
-        if(typeof str === "string"){
-          return true
-        }
-
-        return false
-      }
-
-     /**
-     * @description: Remove the left space.(去除左边所有空格)
-     * @param {string} trimLeftStr A string that needs to be remove all spaces on the left.(需要去除左边空格的字符串)
-     * @example 
-     *  StringTool.trimLeft(' StringTool') // StringTool
-     * @returns {string} Returns a string that removes the left space (去除左边空格之后的字符) 
-     */    
-    static trimLeft(trimLeftStr:string):string {
-      if(!this.#checkIsString(trimLeftStr)){
-        console.warn(`${trimLeftStr}非字符串！`)
-        return trimLeftStr
-      }
-
-      return trimLeftStr.replace(/^\s+/, '');
+  static #checkIsString(str: unknown): str is string {
+    if (typeof str === "string") {
+      return true
     }
 
-     /**
-     * @description: Remove the right space.(去除右边所有空格)
-     * @param {string} trimRightStr A string that needs to be remove all spaces on the right.(需要去除右边空格的字符串)
-     * @example 
-     *  StringTool.trimRight('StringTool ') // StringTool
-     * @returns {string} Returns a string that removes the left space (去除右边空格之后的字符) 
-     */    
-      static trimRight(trimRightStr:string):string {
-        if(!this.#checkIsString(trimRightStr)){
-          console.warn(`${trimRightStr}非字符串！`)
-          return trimRightStr
-        }
-
-        return trimRightStr.replace(/\s+$/, '');
-      }
-
-     /**
-     * @description: Remove the left space and right space.(去除左右所有空格)
-     * @param {string} trimLeftAndRightStr A string that needs to be remove all spaces on the  both side.(需要去除边上空格的字符串)
-     * @example 
-     *  StringTool.trimLeftAndRightStr(' StringTool ') // StringTool
-     * @returns {string} Returns a string that removes the both side space (去除左右两边空格之后的字符) 
-     */    
-      static trimSide(trimLeftAndRightStr:string):string {
-        if(!this.#checkIsString(trimLeftAndRightStr)){
-          console.warn(`${trimLeftAndRightStr}非字符串！`)
-          return trimLeftAndRightStr
-        }
-
-        return trimLeftAndRightStr.trim();
-      }
-
-        /**
-     * @description: Remove the all space(去除所有空格)
-     * @param {string} trimAllStr A string that needs to be remove all spaces.(需要去除所有空格的字符串)
-     * @example 
-     *  StringTool.trimAllStr(' String Tool ') // StringTool
-     * @returns {string} Returns a string that removes all space (去除所有空格之后的字符) 
-     */    
-      static trimAll(trimAllStr:string):string {
-        if(!this.#checkIsString(trimAllStr)){
-          console.warn(`${trimAllStr}非字符串！`)
-          return trimAllStr
-        }
-
-        return trimAllStr.replace(/\s/g,'');
-      }
+    return false
+  }
+  /**
+   * @enum Trim options enum.(去除空格选项枚举)   
+   *    - `all`: Remove all spaces from string. (去除字符串中所有空格)
+   *    - `side`: Remove leading and trailing spaces from string. (去除字符串左右两端空格)
+   *    - `leading`: Remove spaces leading spaces from string. (去除字符串前导空格)
+   *    - `trailing`: Remove spaces trailing spaces from string. (去除字符串尾部空格)
+   */
+  static trimOptions = TrimOptions;
+  /**
+   * @description: Remove spaces from string.(去除字符串中的空格)
+   * @param {string} str  A string that needs to be remove spaces.(需要去除空格的字符串)
+   * @param {TrimOptions} trimOptions Trim options，default is all.(去除空格选项，默认为all) 
+   *    - `all`: Remove all spaces from string. (去除字符串中所有空格)
+   *    - `side`: Remove leading and trailing spaces from string. (去除字符串左右两端空格)
+   *    - `leading`: Remove spaces leading spaces from string. (去除字符串前导空格)
+   *    - `trailing`: Remove spaces trailing spaces from string. (去除字符串尾部空格)
+   * @example
+   * StringTool.trim(' String Tool ')                               //StringTool
+   * StringTool.trim(' StringTool ',StringTool.trimOptions.side)    //StringTool
+   * StringTool.trim('StringTool ',StringTool.trimOptions.trailing) //StringTool
+   * StringTool.trim(' StringTool',StringTool.trimOptions.leading)  //StringTool
+   */
+  static trim(str: string, trimOptions: TrimOptions = TrimOptions.all): string {
+    if (!this.#checkIsString(str)) {
+      console.warn(`${str}非字符串！`)
+      return str
+    }
+    switch (trimOptions) {
+      case TrimOptions.all:
+        return str.replace(/\s/g, '');
+      case TrimOptions.side:
+        return str.trim();
+      case TrimOptions.leading:
+        return str.replace(/^\s+/, '');
+      case TrimOptions.trailing:
+        return str.replace(/\s+$/, '');
+      default:
+        throw new Error("Please pass in the correct TrimOptions parameters");
+    }
+  }
 }
