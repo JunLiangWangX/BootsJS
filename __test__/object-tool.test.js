8b645545fb472897df474d682b54cf2a982cfef8
汪军梁 <1428637884@qq.com>
2024-03-26 18:06:38 +0800

    feat:add argToKey function

--- __test__/object-tool.test.js
+++ __test__/object-tool.test.js
@@ -9,4 +9,116 @@ test('test DateTool class', () => {
     expect(ObjectTool.type(new Map())).toBe('Map');
     expect(ObjectTool.type(new Array())).toBe('Array');
     expect(ObjectTool.type(new ArrayBuffer())).toBe('ArrayBuffer');
+    let arrayBuffer=new ArrayBuffer(10),
+    int16Array=new Int16Array(arrayBuffer),
+    map=new Map(),
+    set=new Set(),
+    set1=new Set(),
+    obj={
+        test:1233,
+        test2:1111
+    },
+    obj_1={
+        test2:1111,
+        test:1233
+    },
+    fn=function(pr){
+        console.info(pr)
+    }
+    weakRef=new WeakRef(obj)
+
+    int16Array[0]=10
+    int16Array[1]=20
+    map.set({a:1},1)
+    map.set({a:2},2)
+    map.set(true,3)
+    map.set("true",4)
+    map.set(13,4)
+
+    
+    set1.add(true)
+    set1.add(13)
+    set1.add({a:2})
+    set1.add({a:1})
+
+    set.add({a:1})
+    set.add({a:2})
+    set.add(13)
+    set.add(true)
+
+    let obj1={
+        array:[1,2,3,4],
+        boolean:false,
+        number:1,
+        string:'2394',
+        null:null,
+        undefined:undefined,
+        bigInt:BigInt(2233),
+        objcet:{
+            arrayBuffer:arrayBuffer,
+            int16Array:int16Array,
+            number:1,
+            obj2:{
+                map:map,
+                set:set,
+                obj:obj,
+                weakRef:weakRef,
+                fn:fn
+            }
+        },
+        number2:1,
+    }
+    let obj2={
+        array:[1,2,3,4],
+        boolean:false,
+        number:1,
+        number2:1,
+        string:'2394',
+        null:null,
+        undefined:undefined,
+        bigInt:BigInt(2233),
+        objcet:{
+            number:1,
+            arrayBuffer:arrayBuffer,
+            int16Array:int16Array,
+            obj2:{
+                map:map,
+                set:set1,
+                obj:obj_1,
+                weakRef:weakRef,
+                fn:fn
+            }
+        }
+    }
+    let obj3={
+        array:[2,2,3,4],
+        boolean:false,
+        number:1,
+        string:'2394',
+        null:null,
+        undefined:undefined,
+        bigInt:BigInt(2233),
+        objcet:{
+            arrayBuffer:arrayBuffer,
+            int16Array:int16Array,
+            obj2:{
+                map:map,
+                set:set1,
+                obj:obj_1,
+                weakRef:weakRef,
+                fn:fn
+            }
+        }
+    }
+    let obj4={}
+    obj4.a={
+        b:'123',
+        c:obj4
+    }
+    console.info(ObjectTool.argToKey(obj4))
+    console.info(ObjectTool.argToKey(obj2))
+
+    expect(ObjectTool.argToKey(obj1)===ObjectTool.argToKey(obj2)).toBe(true);
+    expect(ObjectTool.argToKey(obj1)===ObjectTool.argToKey(obj3)).toBe(false);
+
 });
--- src/object-tool.ts
+++ src/object-tool.ts
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2024-03-21 20:15:10
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2024-03-25 17:47:40
+ * @LastEditTime: 2024-03-26 18:03:40
  */
 
 /**
@@ -24,34 +24,100 @@
 export class ObjectTool {
   /**
    * @description: Determine parameter type.(判断给定参数类型)
-   * @param {*} val Need to determine the type of value.(需要判断类型的值)
+   * @param {*} obj Need to determine the type of value.(需要判断类型的值)
    * @example
    * ObjectTool.type(new Array()); //'Array'
    * ObjectTool.type('123'); //'String'
    * ObjectTool.type(true); //'Boolean'
    * ObjectTool.type(new Map()); //'Map'
    */
-  static type(val: any): string {
-    return Object.prototype.toString.call(val).split(" ")[1].slice(0, -1);
+  static type(obj: any): string {
+    return Object.prototype.toString.call(obj).split(" ")[1].slice(0, -1);
   }
 
   static deepClone(obj: any): any {
     let type = this.type(obj);
-    if (type in PrimitiveTypeEnum) return obj;
-    if (type in TypedArrayEnum) return new obj.constructor(obj);
+    if (type in NumericTypeEnum) return new obj.constructor(obj);
+    if (type === ArrayType) {
+      let tempArray = new Array();
+      for (const item of obj) tempArray.push(this.deepClone(item));
+      return tempArray;
+    }
+    if (type === SetType) {
+      let tempSet = new Set();
+      for (const item of obj) tempSet.add(this.deepClone(item));
+      return tempSet;
+    }
+    return obj
   }
 
   static isEqual(obj1: any, obj2: any): boolean {
     if (obj1 === obj2) return true;
     let type1 = this.type(obj1), type2 = this.type(obj2);
     if (type1 !== type2) return false;
-    // if (PrimitiveTypeEnum.includes(type1)) return obj1 === obj2;
-    if (type1 in TypedArrayEnum) {
+    if (type1 in NumericTypeEnum) return obj1.valueOf().toString() === obj2.valueOf().toString();
+    if (type1 === ArrayType) {
       if (obj1.length !== obj2.length) return false;
-      for (let i = 0; i < obj1.length; i++) if (obj1[i] !== obj2[i]) return false;
-      return true;
+      for (let i = 0; i < obj2.length; i++)if (this.isEqual(obj1[i], obj2[i]) === false) return false
+      return true
+    }
+    if (type1 === SetType) {
+      let tempArray1 = Array.from(obj1), tempArray2 = Array.from(obj2);
+      tempArray1.sort(); tempArray2.sort();
+      if (obj1.length !== obj2.length) return false;
+      for (let i = 0; i < obj2.length; i++)if (this.isEqual(obj1[i], obj2[i]) === false) return false
+      return true
     }
     return false;
+  }
+  static argToKey(arg: any, visited = new Set<any>()): string {
+    let type = Object.prototype.toString.call(arg).split(" ")[1].slice(0, -1);
+    if (visited.has(arg)) return '[Circular Reference]';
+    visited.add(arg);
+    if (type in NumericTypeEnum || type === FunctionType) return arg.toString();
+    if (type === ArrayBufferType) return new Int8Array(arg).toString();
+    if (type === DataViewType) return new Int8Array(arg.buffer).toString();
+    if (type === ObjectType) {
+      let sortArray: string[] = [];
+      let argList: string[] = [];
+      Object.keys(arg).forEach((key) => {
+        sortArray.push(key);
+      });
+      sortArray.sort();
+      for (let item of sortArray) {
+        argList.push(item);
+        argList.push(this.argToKey(arg[item], visited));
+      }
+      visited.delete(arg);
+      return '{' + argList.join(':') + '}';
+    }
+    if (type === WeakRefType) return type + ':{' + this.argToKey(arg.deref(), visited) + '}';
+    if (type === ArrayType || type === SetType) {
+      let strList: string[] = [];
+      for (let item of arg) strList.push(this.argToKey(item, visited));
+      if(type === SetType)strList.sort()
+      visited.delete(arg);
+      return type + ':[' + strList.join(',') + ']';
+    }
+    if (type === MapType) {
+      let sortArray: string[] = [];
+      let keyMap = new Map();
+      let argList: string[] = [];
+      for (const key of arg.keys()) {
+        let genKey = this.argToKey(key, visited);
+        sortArray.push(genKey);
+        keyMap.set(genKey, arg.get(key));
+      }
+      sortArray.sort();
+      for (let item of sortArray) {
+        argList.push(item);
+        argList.push(this.argToKey(keyMap.get(item), visited));
+      }
+      visited.delete(arg);
+      return type + ':{' + argList.join(':') + '}';
+    }
+    visited.delete(arg);
+    return String(arg);
   }
 }
 
@@ -59,47 +125,41 @@ export class ObjectTool {
  * Primitive type enum(原始类型枚举)
  */
 enum PrimitiveTypeEnum {
-  'Number',
-  'String',
-  'Boolean',
-  'Null',
-  'Undefined',
-  'Symbol',
-  'BigInt'
+  Number = 'Number',
+  String = 'String',
+  Boolean = 'Boolean',
+  Null = 'Null',
+  Undefined = 'Undefined',
+  Symbol = 'Symbol',
+  BigInt = 'BigInt'
 };
 
 /**
- * TypedArray type enum(数值数组类型枚举)
+ * Numeric type enum(数值类型枚举)
  */
-enum TypedArrayEnum {
-  "Int8Array",
-  "Int16Array",
-  "Int32Array",
-  "Uint8Array",
-  "Uint16Array",
-  "Uint32Array",
-  "Float32Array",
-  "Float64Array",
-  "BigInt64Array",
-  "BigUint64Array"
+export enum NumericTypeEnum {
+  Int8Array = "Int8Array",
+  Int16Array = "Int16Array",
+  Int32Array = "Int32Array",
+  Uint8Array = "Uint8Array",
+  Uint16Array = "Uint16Array",
+  Uint32Array = "Uint32Array",
+  Float32Array = "Float32Array",
+  Float64Array = "Float64Array",
+  BigInt64Array = "BigInt64Array",
+  BigUint64Array = "BigUint64Array",
+  Date = "Date",
+  RegExp = "RegExp"
 };
-/**
- * Array type enum(数组类型枚举)
- */
-const ArrayTypeEnum = ["Array", "ArrayBuffer", 'DataView'];
-/**
- * Map type enum(Map类型枚举)
- */
-type MapTypeEnum = "Map" | "WeakMap";
-/**
- * Set type enum(Set类型枚举)
- */
-type SetTypeEnum = "Set" | "WeakSet";
-/**
- * Other type enum(其他类型枚举)
- */
-type OtherTypeEnum = "Object" | "Function" | "WeakRef" | "Date" | "RegExp";
-/**
- * Type Enum(类型枚举)
- */
-//type TypeEnum = PrimitiveTypeEnum | ArrayTypeEnum | OtherTypeEnum;
+
+const ArrayType = 'Array'
+const SetType = 'Set'
+const MapType = 'Map'
+const ObjectType = 'Object'
+const ArrayBufferType = 'ArrayBuffer'
+const DataViewType = 'DataView'
+const FunctionType = 'Function'
+const WeakRefType = 'WeakRef'
+
+// 无需处理的属性
+//  "WeakMap"| "WeakSet" | "Function" | "WeakRef"
